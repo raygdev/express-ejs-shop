c04965d769312c3d5799d46830d462bb6cf347f0
raygdev <101662915+raygdev@users.noreply.github.com>
2022-08-14 21:22:25 -0400

    Format products file

--- models/Products.js
+++ models/Products.js
@@ -1,38 +1,35 @@
-const fs = require('fs');
-const path = require('path')
-let p = path.join(path.dirname(require.main.filename), 'data', 'poducts.json');
+const fs = require("fs");
+const path = require("path");
+let p = path.join(path.dirname(require.main.filename), "data", "poducts.json");
 
 module.exports = class Product {
-    constructor(title, price, description){
-        this.title = title
-        this.price = price
-        this.description = description
-    }
+  constructor(title, price, description) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+  }
 
-    save(){
-        fs.readFile(p, (err, fileContent) => {
-            let products = []
-            if(!err){
-                products = JSON.parse(fileContent)
-                return products
-            }
-            products.push(this)
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err)
-            })
-        });
-    }
+  save() {
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+        return products;
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb){
-        fs.readFile(p, (err, fileContent) => {
-            if(err){
-                 cb([])
-            }
-                
-            cb(JSON.parse(fileContent))
-    })
-}
-    
-}
+  static fetchAll(cb) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
 
-
+      cb(JSON.parse(fileContent));
+    });
+  }
+};
